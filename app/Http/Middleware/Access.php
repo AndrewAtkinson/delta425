bb9856fc5e5775bade5d302f796b5c1d3d3f76d8
Andrew Atkinson <andrew.atkinson@visualsoft.co.uk>
2017-02-07 02:04:59 +0000

    auth
    removed auth code

--- app/Http/Middleware/Access.php
+++ app/Http/Middleware/Access.php
@@ -3,27 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Auth\Factory as Auth;
 
 class Access
 {
-  /**
-   * The authentication guard factory instance.
-   *
-   * @var \Illuminate\Contracts\Auth\Factory
-   */
-  protected $auth;
-
-  /**
-   * Create a new middleware instance.
-   *
-   * @param  \Illuminate\Contracts\Auth\Factory $auth
-   * @return void
-   */
-  public function __construct(Auth $auth)
-  {
-    $this->auth = $auth;
-  }
 
   /**
    * Handle an incoming request.
